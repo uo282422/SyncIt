a9cbab139b0bc2e05be17c3aa72e8556a6e00998
uo282422 <98962656+uo282422@users.noreply.github.com>
2024-06-10 19:18:41 +0200

    Arreglos en rama Main

--- src/dbConnection/connection.js
+++ src/dbConnection/connection.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../config/serviceAccountKey.json'); 
+
 require('dotenv').config();
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY))
